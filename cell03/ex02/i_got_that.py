37e564f5cd706d8f898c810e292b32d89c8b9f51
Suwimon249 <67030249@kmitl.ac.th>
2024-11-21 15:37:05 +0000

    02

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,6 +1,6 @@
 def main():
     while True:
-        user_input = input("What you gotta sayM : ")
+        user_input = input("What you gotta say? : ")
         if user_input.strip().upper() == "STOP":
             break
         print("I got that! Anything else? : ")
